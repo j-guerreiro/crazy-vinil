26f7965d5d06e53835da68ffc7a88a7cc47be9b7
jguerreiro <joel.borghi@gmail.com>
2024-01-27 13:07:51 -0300

    Updated controller and homepage

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,20 +2,91 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
 
-class VinylController
+class VinylController extends AbstractController
 {
   /**
+   * The homepage
    * return @response
    */
   #[Route('/')]
   public function homepage(): Response
   {
-    return new Response('Esposinha fedidinha!');
+    $neoclassicalRock = [
+      [
+        "artist" => "Yngwie Malmsteen",
+        "song" => "Black Star"
+      ],
+      [
+        "artist" => "Ritchie Blackmore's Rainbow",
+        "song" => "Stargazer"
+      ],
+      [
+        "artist" => "Joe Satriani",
+        "song" => "Surfing with the Alien"
+      ],
+      [
+        "artist" => "Steve Vai",
+        "song" => "For the Love of God"
+      ],
+      [
+        "artist" => "Jason Becker",
+        "song" => "Altitudes"
+      ],
+      [
+        "artist" => "Tony MacAlpine",
+        "song" => "The Taker"
+      ],
+      [
+        "artist" => "Marty Friedman",
+        "song" => "Thunder March"
+      ],
+      [
+        "artist" => "Michael Romeo",
+        "song" => "The Dark Chapter"
+      ],
+      [
+        "artist" => "Buckethead",
+        "song" => "Jordan"
+      ],
+      [
+        "artist" => "Paul Gilbert",
+        "song" => "Technical Difficulties"
+      ],
+      [
+        "artist" => "John Petrucci",
+        "song" => "Glasgow Kiss"
+      ],
+      [
+        "artist" => "Neo-Classical Orchestra",
+        "song" => "The Rising"
+      ],
+      [
+        "artist" => "Vinnie Moore",
+        "song" => "Lifeforce"
+      ],
+      [
+        "artist" => "Chris Impellitteri",
+        "song" => "17th Century Chicken Pickin'"
+      ],
+      [
+        "artist" => "Uli Jon Roth",
+        "song" => "Sails of Charon"
+      ],
+    ];
+
+    return $this->render(
+      'vinyl/homepage.html.twig',
+      [
+        'title' => 'PB & Jams',
+        'tracks' => $neoclassicalRock
+      ]
+    );
   }
   /**
    * return @response
@@ -23,7 +94,7 @@ class VinylController
   #[Route('/browse/{slug}')]
   public function browse(string $slug = null): Response
   {
-    if($slug) {
+    if ($slug) {
       $title = 'Genre: ' . u(str_replace('-', ' ', $slug))->title(true);
     } else {
       $title = 'All genres!';
